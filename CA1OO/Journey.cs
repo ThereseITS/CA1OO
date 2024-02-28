a32bf2bceb9b05997e562c29d41839b2b226b317
Therese Hume <Therese.Hume@atu.ie>
2024-02-28 16:01:28 +0000

    Add project files.

---
+++ CA1OO/Journey.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1OO
+{
+    internal class Journey
+    {
+       
+            int seatsOccupied = 0;
+            decimal moneyTaken = 0;
+            int numberOfTickets = 0;
+            int numberOfReturns = 0;
+
+            int[] numberBoughtAt;
+            int[] numberBoughtFor;
+
+            int[] minsToFinalDestination;
+            string[] destinations;
+
+            decimal basePrice = 10m;
+            int seatsOnBus = 10;
+            bool reverse = false;
+
+            public Journey(string[] destinations, int[] minsToFinalDestination, bool reverse, decimal basePrice, int seatsOnBus)
+            {
+                numberBoughtAt = new int[destinations.Length];
+                numberBoughtFor = new int[destinations.Length];
+                this.destinations = destinations;
+                this.minsToFinalDestination = minsToFinalDestination;
+                this.basePrice = basePrice;
+                this.seatsOnBus = seatsOnBus;
+                this.reverse = reverse;
+            }
+
+
+        public decimal BuyTicket(string origin, string destination, string ticketType, string customerType)
+        {
+
+            if(seatsOccupied<seatsOnBus)
+            {
+                int indexO = Array.IndexOf(destinations, origin);
+                int indexD = Array.IndexOf(destinations, destination);
+
+                if (indexO >= 0 && indexD >= 0)
+                {
+
+                    decimal price = CalculateBasicPrice(basePrice, minsToFinalDestination[0], minsToFinalDestination[indexO], minsToFinalDestination[indexD]);
+                    price = CalculateTicketPrice(price, ticketType);
+                    price = ApplyDiscount(price, customerType);
+
+                    numberOfTickets++;
+                    seatsOccupied++;
+                    moneyTaken += price;
+                    numberBoughtAt[indexO]++;
+                    numberBoughtFor[indexD]++;
+
+                    return price;
+                }
+            }
+            
+                return -1;
+        
+        }
+
+        public bool CheckInTicket()
+        {
+            if (seatsOccupied < seatsOnBus)
+            {
+                seatsOccupied++;
+                numberOfReturns++;
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public decimal CalculateBasicPrice(decimal basePrice, int totalJourneyTime,int timeFromOrigin, int timeFromDestination)
+        {
+            decimal price = 0;
+
+            decimal pricePerMinute = basePrice / totalJourneyTime;
+
+            price = Math.Abs((timeFromOrigin - timeFromDestination)) * pricePerMinute;
+
+            return price;
+        }
+        public decimal CalculateTicketPrice(decimal price, string ticketType)
+        {
+            if (price < 0m) return -1;
+     
+
+            switch (ticketType.ToLower())
+            {
+                case "single":  break;
+                case "return": price = price * 1.5m; break;
+                default:  break;
+            }
+
+            return price;
+        }
+        static decimal ApplyDiscount(decimal price, string customerType)
+        {
+            if (price < 0) return -1;
+
+            switch (customerType.ToLower())
+            {
+                case "student": price = price * (0.7m); break;
+                case "child": price = price * (0.5m); break;
+                case "oap": price = 0m; break;
+                default: break; // Superflous code- for anything else the price remains unchanged.
+            }
+
+            return price;
+        }
+        public  void PrintJourneyReport()
+        {
+            Console.WriteLine($"\nJourney Report");
+            Console.WriteLine($"______________");
+            Console.WriteLine($"Seats Occupied : {seatsOccupied}");
+            Console.WriteLine($"Money Taken : {moneyTaken}\n");
+            Console.WriteLine($"Tickets Bought: {numberOfTickets}");
+            Console.WriteLine($"Return Tickets: {numberOfReturns}");
+
+            for(int i=0;i<destinations.Length;i++) 
+            {
+                Console.WriteLine($"Tickets bought at {destinations[i]}   = {numberBoughtAt[i]}");
+                Console.WriteLine($"Tickets bought for {destinations[i]}   = {numberBoughtFor[i]}");
+            }
+        }
+
+        
+    }
+}
---
+++ CA1OO/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Text;
+
+namespace CA1OO
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Note the definition of a menu as an array of string
+
+            int choice = 0;
+
+            string[] mainMenu = { "Buy Ticket", "Check in Return ticket", "Print Journey Report", "Exit" };
+            string[] ticketMenu = { "Single", "Return" };
+            string[] customerMenu = { "Adult", "Child", "OAP", "Student" };
+
+            // Basic Journey information - details of the route
+
+           int[] minsToFinalDestination = { 40, 30, 20, 15, 10, 0 }; // travel time to the final destination in minutes.
+           string[] destinations = { "Ballyshannon", "Bundoran", "Cliffoney", "Grange", "Rathcormack", "Sligo" }; // stops on the route.
+
+            //Details of the basic price for a single ticket to the destination
+
+           decimal basePrice = 10m;
+
+            // details about the bus
+
+           int seatsOnBus = 10;
+
+
+            Journey j = new Journey(destinations, minsToFinalDestination, false, 10, 10);
+
+
+            do
+            {
+                choice = GetMenuOption("Ballin Buses:", mainMenu);
+                switch (choice)
+                {
+                    case 0:
+                        string origin = destinations[GetMenuOption("Please enter Origin: ", destinations)];
+                        string destination = destinations[GetMenuOption("Please enter Origin: ", destinations)];
+                        string ticketType = ticketMenu[GetMenuOption("Please enter ticket type: ", ticketMenu)];
+                        string customerType = customerMenu[GetMenuOption("Please enter customer type", customerMenu)];
+                        decimal price = j.BuyTicket(origin, destination,ticketType,customerType);
+                        if (price > 0)
+                        {
+                            Console.WriteLine($" {customerType} {ticketType} from {origin} to {destination} :{price:C}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($" Bus full");
+                        }
+                            break;
+
+                    case 1: if (!j.CheckInTicket()) Console.WriteLine($" Bus full"); ; break;
+                    case 2:j.PrintJourneyReport(); break;
+                    default: break;
+                }
+            }
+            while (choice != mainMenu.Length - 1);
+        }
+
+        /// <summary>
+        /// This method prints out a menu passes as an array of strings and returns the chosen option.
+        /// We could rewrite this to split the header as a seperate string. 
+        /// </summary>
+        /// <param name="menu">a string array of menu options </param>
+        /// <returns> an integer representing the index of the menu item chosen.</returns>
+        static int GetMenuOption(string header, string[] menuOptions)
+        {
+            int choice = 0;
+            Console.WriteLine(header);
+
+
+            while (choice <= 0 || choice > menuOptions.Length)
+            {
+                for (int i = 0; i < menuOptions.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {menuOptions[i]}");
+                }
+                while (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Please enter a valid number");
+                }
+
+
+                if (choice <= 0 || choice > menuOptions.Length)
+                {
+                    Console.WriteLine("Incorrect menu choice");
+                }
+            }
+            return choice - 1;
+
+        }
+    }
+}
